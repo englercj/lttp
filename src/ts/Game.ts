6e208ab75c3c86d9fb6d5ab1b006d454dce2e022
Chad Engler <englercj@live.com>
2014-08-20 20:40:01 -0700

    get scaling working
    Former-commit-id: 53936d0fc6c8a62826a2331145c06059a9fec89d

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -2,7 +2,7 @@ module Lttp {
     export class Game extends Phaser.Game {
 
         constructor() {
-            super(768, 672, Phaser.AUTO, 'game');
+            super(Data.Constants.GAME_WIDTH, Data.Constants.GAME_HEIGHT, Phaser.AUTO, 'game');
 
             this.state.add('Boot', States.Boot, false);
             this.state.add('Preloader', States.Preloader, false);
@@ -15,7 +15,14 @@ module Lttp {
 
         boot() {
             super.boot();
-            // this.scale.setScreenSize(false); //TODO: phaser.d.ts says param is required, but it isn't
+
+            //  This sets a limit on the up-scale
+            this.scale.maxWidth = Data.Constants.GAME_WIDTH * Data.Constants.GAME_SCALE;
+            this.scale.maxHeight = Data.Constants.GAME_HEIGHT * Data.Constants.GAME_SCALE;
+
+            //  Then we tell Phaser that we want it to scale up to whatever the browser can handle, but to do it proportionally
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.setScreenSize(false); //TODO: phaser.d.ts says param is required, but it isn't
         }
 
     }
