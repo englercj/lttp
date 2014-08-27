c1a3ee6b240463a9228cb2de7fdee95f5e3b30f1
Chad Engler <englercj@live.com>
2014-08-27 09:14:29 -0700

    patch scaling to not use Phaser autoscaling
    Former-commit-id: 56051753696d318770c5e1689d4cce2adf944fd7

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -2,9 +2,21 @@ module Lttp {
     export class Game extends Phaser.Game {
 
         constructor() {
-            super(Data.Constants.GAME_WIDTH, Data.Constants.GAME_HEIGHT, Phaser.AUTO, 'game');
+            // super(Data.Constants.GAME_WIDTH, Data.Constants.GAME_HEIGHT, Phaser.AUTO, 'game');
 
-            // PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+            //////////////////////////////////////////////////////
+            //issue: https://code.google.com/p/chromium/issues/detail?id=134040
+            // When a chrome bug that prevents CSS scaling from being NEAREST filtered is fixed, remove this
+            // and uncomment above.
+            super(
+                Data.Constants.GAME_WIDTH * Data.Constants.GAME_SCALE,
+                Data.Constants.GAME_HEIGHT * Data.Constants.GAME_SCALE,
+                Phaser.AUTO,
+                'game'
+            );
+
+            PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+            //////////////////////////////////////////////////////
 
             this.state.add('Boot', States.Boot, false);
             this.state.add('Preloader', States.Preloader, false);
@@ -18,16 +30,23 @@ module Lttp {
         boot() {
             super.boot();
 
-            // This sets the limits for Phaser's auto scaling
-            this.scale.minWidth = Data.Constants.GAME_WIDTH;
-            this.scale.minHeight = Data.Constants.GAME_HEIGHT;
+            // // This sets the limits for Phaser's auto scaling
+            // this.scale.minWidth = Data.Constants.GAME_WIDTH;
+            // this.scale.minHeight = Data.Constants.GAME_HEIGHT;
 
-            this.scale.maxWidth = Data.Constants.GAME_WIDTH * Data.Constants.GAME_SCALE;
-            this.scale.maxHeight = Data.Constants.GAME_HEIGHT * Data.Constants.GAME_SCALE;
+            // this.scale.maxWidth = Data.Constants.GAME_WIDTH * Data.Constants.GAME_SCALE;
+            // this.scale.maxHeight = Data.Constants.GAME_HEIGHT * Data.Constants.GAME_SCALE;
 
-            // Then we tell Phaser that we want it to scale between those values, the largest the browser can fit
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.setScreenSize();
+            // // Then we tell Phaser that we want it to scale between those values, the largest the browser can fit
+            // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            // this.scale.setScreenSize();
+
+            //////////////////////////////////////////////////////
+            //issue: https://code.google.com/p/chromium/issues/detail?id=134040
+            // When a chrome bug that prevents CSS scaling from being NEAREST filtered is fixed, remove this
+            // and uncomment above.
+            this.world.scale.set(Data.Constants.GAME_SCALE);
+            //////////////////////////////////////////////////////
 
             // Phaser by default mutes sound on pause, but I actually want to pause
             // it so it can resume from the same spot.
