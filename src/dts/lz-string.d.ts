4d46ee6bd3d997aff8de05fb970a80b4d5a7d10c
Chad Engler <englercj@live.com>
2015-04-18 14:13:48 -0700

    add definitions for lz-string
    Former-commit-id: 6dfcb7454dd9e1d12b2834623079b576b932f0c9

---
+++ src/dts/lz-string.d.ts
@@ -0,0 +1,4 @@
+declare module LZString {
+    function compressToUTF16(str: string): string;
+    function decompressFromUTF16(str: string): string;
+}
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,4 +1,5 @@
 /// <reference path="../dts/phaser.d.ts" />
+/// <reference path="../dts/lz-string.d.ts" />
 
 var game;
 window.onload = () => {
