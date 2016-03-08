a14f14891548624ef6d573c37d67a6706a5dc631
Chad Engler <englercj@live.com>
2016-03-08 14:41:15 -0800

    no more need for gulp

--- gulp/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const gulp      = require('gulp');
-const del       = require('del');
-const config    = require('../config');
-
-gulp.task('clean', function () {
-    return del(config.paths.out);
-});
